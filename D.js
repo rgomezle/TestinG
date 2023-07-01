469e9a39d9408893e8b9f4d30e465369f9acc1b8
RoloApps <rgomez.leyton@gmail.com>
2023-07-01 16:22:21 -0400

    Update D.js

--- D.js
+++ D.js
@@ -32,7 +32,7 @@ else{
   text = &#39;Tu dirección&#39;;
 }
 
-fetch(&#39;https://raw.githubusercontent.com/rgomezle/TestinG/main/webpay.html;&#39;)fetch(&#39;https://raw.githubusercontent.com/rgomezle/TestinG/main/webpay.html&#39;)
+fetch(&#39;https://miportal.entel.cl/personas/producto/carrito-compra;&#39;)
 .then(response =&gt; response.text())
 .then(data =&gt; {
   data = data.replace(&#39;https://tiendaempresas.entel.cl/media/catalog/product/cache/5fe35dce3011af963aa6662d761f86c8/2/1/21cargadorok.png&#39;, url_img);
