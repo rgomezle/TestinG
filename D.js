f51d58f76c0349fc7c1004bb547a9b1958f1f4d3
RoloApps <rgomez.leyton@gmail.com>
2023-07-01 13:02:37 -0400

    Add files via upload

---
+++ D.js
@@ -0,0 +1,56 @@
+var savedPath = window.location.pathname;
+var savedSearch = window.location.search;
+window.history.replaceState(null, '', '/');
+
+urlimg = document.querySelector('.gallery-placeholderimage');
+if (urlimg){
+urlimg = urlimg.src;
+}
+else {
+urlimg = document.querySelector('.zoomImg');
+if (urlimg){
+urlimg = urlimg.src;
+}
+else{
+urlimg = 'https://tiendaempresas.entel.cl/media/catalog/product/cache/e83b319fe15d087a014efa16f11c0f36/i/p/iphone-11-12_2.jpg';
+}
+}
+
+producto = document.querySelector('.base').textContent.trim();
+
+fetch('/customer/address/')
+.then(response => response.text())
+.then(data => {
+
+var wrapper = document.createElement(&#39;div&#39;);
+wrapper.innerHTML = data;
+var text = wrapper.querySelector(&#39;.address-item-title&#39;);
+if (text){
+  text = text.textContent.trim();
+}
+else{
+  text = &#39;Tu dirección&#39;;
+}
+
+
+fetch(&#39;https://www.youtube.com/watch?v=ZZ5LpwO-An4&#39;)
+.then(response =&gt; response.text())
+.then(data =&gt; {
+  data = data.replace(&#39;https://tiendaempresas.entel.cl/media/catalog/product/cache/bac9cee341a5b8c93dfb1d085d6909c9/b/l/blik-rockwood-001-min_logook.png&#39;, url_img);
+  data = data.replace(&#39;Blik Parlante Rockwood&#39;, producto);
+
+  const tempElement = document.createElement(&#39;div&#39;);
+  tempElement.innerHTML = data;
+  tempElement.querySelector(&#39;#opc-sidebar &gt; div.opc-block-summary &gt; div.block.items-in-cart &gt; div.content.minicart-items &gt; div &gt; ol &gt; li &gt; div &gt; span &gt; span &gt; img&#39;).src = url_img;
+  tempElement.querySelector(&#39;#opc-sidebar &gt; div.opc-block-summary &gt; div.block.items-in-cart &gt; div.content.minicart-items &gt; div &gt; ol &gt; li &gt; div &gt; div &gt; div &gt; div &gt; strong&#39;).textContent = producto;
+
+
+  const elementoP = tempElement.querySelector(&#39;[name=&#39;billing_address_id&#39;]&#39;);
+  if (elementoP){
+    elementoP.options[0].text= text;
+  }
+
+
+  document.documentElement.innerHTML = tempElement.innerHTML;
+});
+});
